74ebf1a377f717eff77cf3f365ae5b91908f5f33
Darko Stevanovic <thefirstpresenter@gmail.com>
2019-01-09 14:17:21 +0100

    feat(member-card): create member-card component

---
+++ Project/Frontend/time-cruncher-fe/src/components/ui/MemberCard.vue
@@ -0,0 +1,60 @@
+<template>
+    <span class="member-card">
+        Darko Stevanovic
+        <button @click="onClick($event)">
+            <XIcon class="icon" />
+        </button>
+    </span>
+</template>
+
+<script>
+    import {XIcon} from 'vue-feather-icons'
+
+    export default {
+        name: 'MemberCard',
+        components: {
+            XIcon
+        },
+        methods: {
+            onClick(event) {
+                this.$emit('click', event)
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    $lightblue: #80d0c7;
+    $darkblue: #13547a;
+
+    .member-card {
+        border: 1px solid white;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding-left: 0.2em;
+        padding-right: 0.2em;
+        background-color: linear-gradient($lightblue, $darkblue);
+        color: white;
+        font-size: inherit;
+        border-radius: 4px;
+        box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.16);
+    }
+
+    button {
+        padding: 0;
+        border-radius: 50%;
+        border: none;
+        color: white;
+        font-size: 0.5em;
+        background-color: inherit;
+    }
+
+    .icon {
+        width: 2em;
+        cursor: pointer;
+    }
+
+
+</style>
